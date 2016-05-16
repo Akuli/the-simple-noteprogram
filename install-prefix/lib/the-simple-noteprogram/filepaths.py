e45a4464b326a69137d2bb8fda1394774df218ee
Akuli <akuviljanen17@gmail.com>
2016-05-16 13:11:32 +0300

    Create filepaths.py

---
+++ install-prefix/lib/the-simple-noteprogram/filepaths.py
@@ -0,0 +1,9 @@
+"""The filepaths"""
+from os.path import abspath, dirname, join
+import appdirs
+from . import about
+
+configdir = appdirs.user_config_dir(about.DASHES)
+install_prefix = dirname(dirname(dirname(abspath(__file__))))
+libdir = join(install_prefix, 'lib')
+sharedir = join(install_prefix, 'share')
