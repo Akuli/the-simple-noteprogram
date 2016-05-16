5c228a0d3f00ac38c8d1736ab63b4edae9a40c17
Akuli <akuviljanen17@gmail.com>
2016-05-16 13:13:27 +0300

    Create __main__.py

---
+++ install-prefix/lib/the-simple-noteprogram/__main__.py
@@ -0,0 +1,40 @@
+"""The main file"""
+import sys
+if sys.version_info[:2] < (3, 2):                           # NOQA
+    sys.exit("This program requires Python 3.2 or newer.")  # NOQA
+import gettext
+from os.path import join
+import signal
+import gi
+gi.require_version('Gtk', '3.0')                            # NOQA
+gi.require_version('AppIndicator3', '0.1')                  # NOQA
+from gi.repository import Gtk
+from . import filepaths
+
+
+# Internationalization
+gettext.bindtextdomain(
+    'messages', localedir=join(filepaths.sharedir, 'locale'),
+)
+gettext.textdomain('messages')
+
+# The icon directory is probably not in Gtk.IconTheme's search_path
+theme = Gtk.IconTheme.get_default()
+if join(filepaths.sharedir, 'icons') not in theme.get_search_path():
+    theme.append_search_path(join(filepaths.sharedir, 'icons'))
+
+# Ctrl+C interrupting, doesn't save currently opened notes
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+
+def main():
+    """Runs the program"""
+    from . import indicator, notes
+    indicator.load()
+    notes.load()
+    Gtk.main()
+    notes.unload()
+    indicator.unload()
+
+if __name__ == '__main__':
+    main()
