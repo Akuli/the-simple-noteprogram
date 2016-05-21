4a579d2fb2114940dc365cfd4ec5cea789900544
Akuli <akuviljanen17@gmail.com>
2016-05-21 21:48:43 +0300

    Add locking

--- install-prefix/lib/the-simple-noteprogram/the_simple_noteprogram/__main__.py
+++ install-prefix/lib/the-simple-noteprogram/the_simple_noteprogram/__main__.py
@@ -1,42 +1,52 @@
 """The main file"""
 import sys
-if sys.version_info[:2] < (3, 2):                           # NOQA
-    sys.exit("This program requires Python 3.2 or newer.")  # NOQA
+if sys.version_info[:2] < (3, 2):
+    sys.exit("This program requires Python 3.2 or newer.")
 import gettext
-import os.path
+import os
 import signal
 import gi
-gi.require_version('Gtk', '3.0')                            # NOQA
-gi.require_version('AppIndicator3', '0.1')                  # NOQA
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from . import about, filepaths
-
-
-# Internationalization
-gettext.bindtextdomain(
-    'the-simple-noteprogram',
-    localedir=os.path.join(filepaths.sharedir, 'locale'),
-)
-gettext.textdomain('the-simple-noteprogram')
-
-# The icon directory is probably not in Gtk.IconTheme's search_path
-theme = Gtk.IconTheme.get_default()
-icondir = os.path.join(filepaths.sharedir, 'icons')
-if icondir not in theme.get_search_path():
-    theme.append_search_path(icondir)
-
-# Ctrl+C interrupting, doesn't save currently opened notes
-signal.signal(signal.SIGINT, signal.SIG_DFL)
+from . import filepaths, locker
 
 
 def main():
     """Runs the program"""
-    from . import indicator, notes
-    indicator.load()
-    notes.load()
-    Gtk.main()
-    notes.unload()
-    indicator.unload()
+    # Checking for another instance
+    if locker.duplicatecheck():
+        dialog = Gtk.MessageDialog(
+            # Setting None as the parent is usually not a good idea, but
+            # in this case there is no parent window
+            None, 0, Gtk.MessageType.INFO, Gtk.ButtonsType.OK,
+            "The Simple Noteprogram is already running.",
+        )
+        dialog.run()
+        dialog.destroy()
+        return
+
+    # Making sure the icon directory is in GTK's search path
+    icondir = os.path.join(filepaths.sharedir, 'icons')
+    theme = Gtk.IconTheme.get_default()
+    if icondir not in theme.get_search_path():
+        theme.append_search_path(icondir)
+
+    # Ctrl+C interrupting, doesn't save opened notes
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    # Internationalization
+    gettext.bindtextdomain('the-simple-noteprogram',
+                           os.path.join(filepaths.sharedir, 'locale'))
+    gettext.textdomain('the-simple-noteprogram')
+
+    with locker.lockfile():
+        from . import indicator, notes
+        indicator.load()
+        notes.load()
+        Gtk.main()
+        notes.unload()
+        indicator.unload()
+
 
 if __name__ == '__main__':
     main()
---
+++ install-prefix/lib/the-simple-noteprogram/the_simple_noteprogram/locker.py
@@ -0,0 +1,27 @@
+"""This file makes sure only one instance is running at a time"""
+import contextlib
+import os
+import tempfile
+import psutil
+
+
+LOCKPATH = os.path.join(tempfile.gettempdir(), 'the-simple-noteprogram-lock')
+
+def duplicatecheck():
+    """Returns True if another instance is running or False if not using
+    a lockfile"""
+    try:
+        with open(LOCKPATH, 'r') as f:
+            pid = int(f.read())
+    except (FileNotFoundError, ValueError):  # invalid or no pid file
+        return False
+    return os.getpid() != pid and psutil.pid_exists(pid)
+
+
+@contextlib.contextmanager
+def lockfile():
+    """A context manager for creating and removing a lock file"""
+    with open(LOCKPATH, 'w') as f:
+        print(os.getpid(), file=f)
+    yield
+    os.remove(LOCKPATH)
